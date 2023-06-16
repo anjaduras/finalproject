d2c0426d0636c26fbfcafad9d6ef9c658a97feb3
anjaduras <anna@duras.org>
2023-06-16 11:51:14 +0200

    Small change

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Game() {
   return (
     <div>
-      <h1>Welcome to the Game!</h1>
+      <h1>Welcome to the Game !</h1>
       <p>This is the game content.</p>
     </div>
   );
